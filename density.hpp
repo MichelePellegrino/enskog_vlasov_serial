682f3c52f928e5134c6c79bec86329228be12aa7
Michele Pellegrino <michele2.pellegrino@mail.polimi.it>
2019-09-25 19:25:01 +0200

    Density kernel partially documented

--- density.hpp
+++ density.hpp
@@ -1,3 +1,7 @@
+/*! \file density.hpp
+ *  \brief Header containing the class implementing density kernel
+ */
+
 #ifndef EV_DENSITIES_HPP
 #define EV_DENSITIES_HPP
 
@@ -8,41 +12,45 @@
 
 #include <cmath>
 
-// Contents:
-//  1) references to: 1.1) grid, 1.2) ensemble
-//  2) no. particles per cell (and function for binning)
-//  3) number density (and num_dens_dummy ???)
-//  4) eta and aveta...
-
-/*! \class DensityCalculator
+/*! \class DensityKernel
  *  \brief Class for density and reduced density computation
+ *
+ *  It stores the values of averaging weights, binned particles, number density
+ *  (reduced and averaged). Moreover, it encapsulates the convolutioner for
+ *  averages computation. Finally, defines map buffer to locate particles in and
+ *  neighbouring a given cell.
  */
 class DensityKernel : protected Motherbase
 {
 
 private:
 
-  // Utilities (maybe reference would be better?)
-  real_number reduce_factor;
-  real_number ns_x, ns_y;
+  // Utilities (maybe references would be better?)
+  real_number reduce_factor;    /*!< Specific covolume (to reduce number density) */
+  real_number ns_x, ns_y;       /*!< Half-number of weights in each direction     */
 
-  ev_matrix::SlideMaskMatrix<real_number> stencil_x;
-  ev_matrix::SlideMaskMatrix<real_number> stencil_y;
-  ev_matrix::SlideMaskMatrix<real_number> weights;
+  ev_matrix::SlideMaskMatrix<real_number> stencil_x;  /*!< Stencil values along x to compute averaging weights  */
+  ev_matrix::SlideMaskMatrix<real_number> stencil_y;  /*!< Stencil values along y to compute averaging weights  */
+  ev_matrix::SlideMaskMatrix<real_number> weights;    /*!< Weights to average density values                    */
 
-  real_number x_min, y_min;
-  int n_cutoff_x, n_cutoff_y;
+  // Utilities (maybe references would be better?)
+  real_number x_min, y_min;     /*!< Grid boundaries                              */
+  int n_cutoff_x, n_cutoff_y;   /*!< Maximum cutoff (needed to reserve halo size) */
 
   // Problem: casting Eigen datatype -> solved!
 
-  ev_matrix::MaskMatrix<int> n_part_cell;
-  ev_matrix::HaloMaskMatrix<real_number> num_dens_cell;
-  ev_matrix::HaloMaskMatrix<real_number> reduced_density;
-  ev_matrix::MaskMatrix<real_number> average_reduced_density;
+  ev_matrix::MaskMatrix<int> n_part_cell;                         /*!< Number of particles for each cell            */
+  ev_matrix::HaloMaskMatrix<real_number> num_dens_cell;           /*!< Number density value in each cell            */
+  ev_matrix::HaloMaskMatrix<real_number> reduced_density;         /*!< Reduced density values in each cell          */
+  ev_matrix::MaskMatrix<real_number> average_reduced_density;     /*!< Averaged reduced density values in each cell */
 
-  ev_matrix::MatrixConvolutioner<real_number> avg_convolutioner;
+  ev_matrix::MatrixConvolutioner<real_number> avg_convolutioner;  /*!< Convolutioner computing averaged density     */
 
-  // Particle-cell map buffers
+  // PARTICLES-CELL MAP BUFFERS
+  /*!
+   *  DensityKernel class defines maps to locate particles. They are obtained by
+   *  computing cumulated density values (putting cells in lexico-graphic order).
+   */
   std::vector<int> idx_cell, idx_map, cum_num, raw_num;
 
   void compute_ind_map_part(void);
@@ -53,18 +61,20 @@ public:
   DensityKernel(DSMC*);
   ~DensityKernel() = default;
 
+  // Each step of density kernel
   void binning (void);
   void fill_dummy_field (void);
   void compute_reduced_density (void);
   void compute_avg_density (void);
+
+  // Density kernel in a packet
   void perform_density_kernel (void);
 
+  // GETTERS
   inline int get_n_cutoff_x(void) const { return n_cutoff_x; }
   inline int get_n_cutoff_y(void) const { return n_cutoff_y; }
-
   inline ev_matrix::HaloMaskMatrix<real_number>& get_num_dens_cell(void) { return num_dens_cell; }
   inline const ev_matrix::HaloMaskMatrix<real_number>& get_num_dens_cell(void) const { return num_dens_cell; }
-
   inline std::vector<int>& get_idx_cell() { return idx_cell; }
   inline const std::vector<int>& get_idx_cell() const { return idx_cell; }
   inline std::vector<int>& get_idx_map() { return idx_map; }
@@ -73,24 +83,22 @@ public:
   inline const std::vector<int>& get_cum_num() const { return cum_num; }
   inline std::vector<int>& get_raw_num() { return raw_num; }
   inline const std::vector<int>& get_raw_num() const { return raw_num; }
-
   inline const int get_npc(int i, int j) const { return n_part_cell(i,j); }
   inline const real_number get_aveta(int i, int j) const { return average_reduced_density(i,j); }
   inline const real_number get_numdens(int i, int j) const { return num_dens_cell(i,j); }
-
   inline const ev_matrix::MaskMatrix<int>& get_npc(void) const { return n_part_cell; }
   inline const ev_matrix::MaskMatrix<real_number>& get_aveta(void) const { return average_reduced_density; }
-
   inline const ev_matrix::SlideMaskMatrix<real_number>& get_weights(void) { return weights; }
-
   inline const int iof(int k) const { return cum_num[k]; }
   inline const int ind(int k) const { return idx_map[k]; }
 
   // DEBUG
+  // # # # # #
   void print_binned_particles(void) const;
   void print_reduced_numdens(void) const;
   void print_reduced_aveta(void) const;
   void test_particle_cell_map(void) const;
+  // # # # # #
 
 };
 
